d6006c34aa881a4afac3665af2f06bbc8805337d
Tomixer <tomix.klokar@gmail.com>
2023-10-20 22:18:29 +0200

    added function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import {useState} from "react"
 
 const App = () => {
+  const[fullName,setFullName]=useState("")
+  const[email,seEmail]=useState("")
+  const[users,setUsers]=useState([])
+
+  const formSubmit=(event)=>{
+    event.preventDefault()
+    console.log("form")
+  }
+
   return <article>
-    <form>
+    <form onSubmit={formSubmit}>
       <input className="userinfo" type="text" placeholder="jmeno" />
       <input className="userinfo" type="email" placeholder="email" />
       <input type="submit" />
