a440c0f23ae3f7c3962929d59b53dceb451cd318
Tomixer <tomix.klokar@gmail.com>
2023-10-21 16:18:44 +0200

    added input

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { useState } from "react";
 const App = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
   const [users, setUsers] = useState([]);
 
   const formSubmit = (event) => {
     event.preventDefault();
 
     if (fullName && email) {
-      const oneUser = { fullName: fullName, email: email };
+      const oneUser = { fullName: fullName, email: email, age: age };
       setUsers((users) => {
         return [...users, oneUser];
       });
@@ -18,6 +19,7 @@ const App = () => {
     }
     setFullName("");
     setEmail("");
+    setAge("");
   };
 
   return (
@@ -37,14 +39,24 @@ const App = () => {
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
+        <input
+          className="userinfo"
+          type="text"
+          placeholder="vek"
+          value={age}
+          onChange={(event) => setAge(event.target.value)}
+        />
         <input type="submit" />
       </form>
-      {users.map((oneUser,index)=>{
-        const{fullName,email}=oneUser
-        return  <div key={index} className="item">
-          <h2>{fullName}</h2>
-          <h2>{email}</h2>
-        </div>
+      {users.map((oneUser, index) => {
+        const { fullName, email, age } = oneUser;
+        return (
+          <div key={index} className="item">
+            <h2>{fullName}</h2>
+            <h2>{email}</h2>
+            <h2>{age}</h2>
+          </div>
+        );
       })}
     </article>
   );
